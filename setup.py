d60dbea39044af2648df4fd2e4f77a3bb35d3bff
Douglas Meehan <dmeehan@gmail.com>
2011-09-13 18:32:22 -0400

    Removed unnecessary files. getting ready to package.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import projects
+import os
+
+try:
+    long_description = open('README.rst').read()
+except IOError:
+    long_description = ''
+
+try:
+    reqs = open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).read()
+except (IOError, OSError):
+    reqs = ''
+
+setup(
+    name='actionmanual',
+    version=projects.get_version(),
+    description='Documenting ideas and projects on vacant land',
+    long_description=long_description,
+    author='Douglas Meehan',
+    author_email='dmeehan@gmail.com',
+    include_package_data=True,
+    url='http://github.com/dmeehan/actionmanual',
+    packages=find_packages(),
+    classifiers=[
+        'Framework :: Django',
+    ],
+    install_requires = reqs,
+    dependency_links = []
+)
