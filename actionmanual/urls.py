ff29f22d1f3a71460233e147dc9440e497e64a0d
Douglas Meehan <dmeehan@gmail.com>
2011-09-14 15:54:29 -0400

    fixed url imports

--- actionmanual/urls.py
+++ actionmanual/urls.py
@@ -6,59 +6,23 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
-
-    # django-grapelli custom admin interface
-    (r'^grappelli/', include('grappelli.urls')),
-    
-    # admin-tools
-    url(r'^admin_tools/', include('admin_tools.urls')),
-    
-    # tinymce admin extension
-    #(r'^tinymce/', include('tinymce.urls')),
-    
-    # registration
-    (r'^accounts/', include('registration.backends.default.urls')),
-    
-    # Profiles
-    (r'^profiles/', include('profiles.urls')),
-    
-    #Comment
-    (r'^flatblocks/', include('flatblocks.urls')),
-    
-    #Search
-    (r'^search/', include('haystack.urls')),
-    
     # home page
     url(r'^$', 'actionmanual.home.views.index', name="home"),
 
-    #Comment
-    (r'^comments/', include('django.contrib.comments.urls')),
-    
     # Portfolio:
     (r'^share/', include('actionmanual.portfolio.urls.share')),
     (r'^explore/', include('actionmanual.portfolio.urls.explore')),
-    
+
     # Resources:
     (r'^resources/', include('actionmanual.resources.urls')),
 
     # About:
     url(r'^about/', 'actionmanual.about.views.about_index', name="about"),
-    
+
     # Contacts:
     (r'^contacts/', include('actionmanual.contacts.urls')),
-    
+
     # Posts:
     (r'^posts/', include('actionmanual.posts.urls')),
-    
-     #Tags:
-    (r'^tags/$',
-      'django.views.generic.list_detail.object_list',
-      { 'queryset': Tag.objects.all() }),
- 
+
 )
